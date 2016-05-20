542c43d17c38176dffa0dfe21a9514f6eb720b96
Chris Grimmett <chris@grimtech.net>
2016-05-20 03:41:38 -0700

    progress on allowing onename

--- api/controllers/online_controller.js
+++ api/controllers/online_controller.js
@@ -12,11 +12,11 @@
  */
 var OpenBazaarAPI = require('insane-openbazaar-api');
 
-
-for (var i=0; i<Object.keys(process.env).length; i++) {
-  if( Object.keys(process.env)[i].substring(0, 3) == 'OB_')
-    console.log('%s=%s', Object.keys(process.env)[i], process.env[Object.keys(process.env)[i]]);
-}
+//
+// for (var i=0; i<Object.keys(process.env).length; i++) {
+//   if( Object.keys(process.env)[i].substring(0, 3) == 'OB_')
+//     console.log('%s=%s', Object.keys(process.env)[i], process.env[Object.keys(process.env)[i]]);
+// }
 
 var ob = new OpenBazaarAPI({
   "host": process.env.OB_HOST,
@@ -43,6 +43,14 @@ module.exports = {
   isOnline: isOnline
 };
 
+
+function resolveOneName(req, res) {
+
+}
+
+
+
+
 /*
   Functions in a127 controllers used for operations should take two parameters:
 
@@ -53,17 +61,24 @@ function isOnline(req, res) {
   // variables defined in the Swagger document can be referenced using req.swagger.params.{parameter_name}
   //console.log(req.swagger.params)
   var guid = req.swagger.params.guid.value || '';
+
+
   var online;
 
   if (!guid) return res.status(400).json({"error": {"code": 1, "message": "no guid was received!"}});
   //var hello = util.format('Hello, %s!', name);
 
-  // @todo #todo - make call
   ob.get('profile', guid, function(err, reply) {
 
     if (err) {
       console.log(err);
-      return res.status(500).json({"error": {"code": 2, "message": err.message}});
+      return res.status(500).json({
+        "online": false,
+        "error": {
+          "code": 2,
+          "message": err.message
+        }
+      });
     }
 
     // assume offline if no profile retrieved
--- test/api/controllers/online_controller.js
+++ test/api/controllers/online_controller.js
@@ -6,13 +6,43 @@ var assert = require('chai').assert;
 describe('controllers', function() {
 
   describe('online_controller', function() {
+    this.timeout(8000);
+
+    describe('GET /v1/online/@openbazaar', function() {
+
+      it('should reject onename handles sent without an @ sign', function(done) {
+        request(app)
+          .get('/v1/online/openbazaar')
+          .set('Accept', 'application/json')
+          .expect('Content-Type', /json/)
+          .expect(200)
+          .end(function(err, res) {
+            assert.isNull(err);
+            assert.match(res.body.error, /at \(\@\) sign required/);
+            done();
+          });
+      });
+    })
 
     describe('GET /v1/online/a06aa22a38f0e62221ab74464c311bd88305f88c', function() {
 
-      it('should return the @openbazaar profile as JSON', function(done) {
+      it('should accept @openbazaar GUID and return profile as JSON', function(done) {
 
         request(app)
           .get('/v1/online/a06aa22a38f0e62221ab74464c311bd88305f88c')
+          .set('Accept', 'application/json')
+          .expect('Content-Type', /json/)
+          .expect(200)
+          .end(function(err, res) {
+            assert.isNull(err);
+            assert.equal(res.profile.handle, '@openbazaar');
+            done();
+          });
+      });
+
+      it('should accept @openbazaar onename handel and return profile as JSON', function(done) {
+        request(app)
+          .get('/v1/online/@openbazaar')
           .set('Accept', 'application/json')
           .expect('Content-Type', /json/)
           .expect(200)
